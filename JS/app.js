eecad7194e975b6216348d51b7e89c516cd22ba1
shishir <shiishir@gmail.com>
2021-09-02 05:13:29 +0600

    Search option and search error and search results done with html

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,50 @@
-console.log('I am in');
+const searchBook = () => {
+    const searchField = document.getElementById("search-field");
+    const searchText = searchField.value;
+    //clear data
+    searchField.value = "";
+    if (searchText === '') {
+        const searchNotFound = document.getElementById('search-not-found');
+        searchNotFound.style.display = 'block';
+    } else {
+        // load data
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => displaySearchResult(data.docs));
+        const searchNotFound = document.getElementById('search-not-found');
+        searchNotFound.style.display = 'none';
+    }
+
+};
+
+const displaySearchResult = (books) => {
+    console.log(books);
+    const searchResult = document.getElementById("search-result");
+    searchResult.textContent = '';
+    if (books.length == 0) {
+        const searchNothingFound = document.getElementById('search-nothing-found');
+        searchNothingFound.style.display = 'block';
+    }
+    else {
+        const searchNothingFound = document.getElementById('search-nothing-found');
+        searchNothingFound.style.display = 'none';
+        books.forEach((book) => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+        <div onclick="" class="card">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}.jpg" class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">${book.title}</h5>
+                <h6 class="card-title">Author: ${book.author_name}</h6>
+                 <p class="card-text">Publisher: ${book.publisher}</p>
+                 <p class="card-text">First Published ${book.first_publish_year}</p>
+          </div>
+        </div>
+    `;
+            searchResult.appendChild(div);
+        });
+    }
+
+};
